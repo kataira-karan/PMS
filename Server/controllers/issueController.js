a3ec8c511f6633fb89545ce88c1538f3f04d924a
karankataria <karankatariac22@gmail.com>
2023-07-13 15:16:31 -0400

    Baord created

--- Server/controllers/issueController.js
+++ Server/controllers/issueController.js
@@ -4,7 +4,6 @@ const Issue = require("../model/issueModel");
 // @desc       Add Issue to a project
 // @route      GET /project/issue/addissue
 // @access     Private
-
 const addIssue = async (req, res) => {
   //   console.log(req);
   const { projectKey } = req.params;
@@ -48,4 +47,32 @@ const addIssue = async (req, res) => {
   }
 };
 
-module.exports = { addIssue };
+// @desc       Add Issue to a project
+// @route      POST /project/issue/updateIssue
+// @access     Private
+const updateIssue = async (req, res) => {
+  console.log("Updating Issue");
+  const { issueId } = req.params;
+  const { name, description, status } = req.body.issue;
+  console.log(req.body);
+
+  try {
+    const issue = await Issue.findOne({ _id: issueId });
+
+    console.log(issue);
+    // ASSIGNING NEW VALUE TO OLD ISSUE VALUES
+    issue.name = name;
+    issue.description = description;
+    issue.status = status;
+
+    // SAVING DOC
+    await issue.save();
+
+    //
+    res.status(200).json({ success: true, issue: issue });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error });
+  }
+};
+
+module.exports = { addIssue, updateIssue };
